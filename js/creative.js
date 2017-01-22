f2e7f5f942020bed106127da90f6a41d997d42d4
Alex Zanetti de Lima <azlima@daitangroup.com>
2017-01-22 04:00:58 -0200

    Setup tiba.art.br first version

--- js/creative.js
+++ js/creative.js
@@ -40,7 +40,20 @@
         offset: {
             top: 100
         }
-    })
+    });
+
+   // Map scrolling behaviour
+    $(document).ready(function() {
+        $('#map_iframe').addClass('scrolloff');
+        $('#map').on('click', function () {
+            $('#map_iframe').removeClass('scrolloff');
+        });
+  
+        $('#map_iframe').mouseleave(function  () {
+            $('#map_iframe').addClass('scrolloff');
+        });
+    });
+  
 
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
